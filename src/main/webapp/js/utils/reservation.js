d8143f06a6a5067330eb1de43e14b82c271f6861
KevinFloresG <kevinfg1413@gmail.com>
2021-04-16 20:26:06 -0600

    Reservations Solo falta el search

--- src/main/webapp/js/utils/reservation.js
+++ src/main/webapp/js/utils/reservation.js
@@ -3,8 +3,13 @@ async function reservationsByUser(username){
     return await response.json()
 }
 
+async function getReservation(id){
+    let response = await fetch("/AirlineJK/reservations/get?id="+id)
+    return await response.json()
+}
+
 function checkinStatus(r){
     return r.seatQuantity !== r.checkedInQuantity?"Pendiente":"Realizado"
 }
 
-export {reservationsByUser, checkinStatus};
+export {reservationsByUser, checkinStatus, getReservation};
