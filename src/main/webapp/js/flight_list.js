6010b3cc91dbc7e1fb0f1c3a05df7b3c55e5d521
KevinFloresG <kevinfg1413@gmail.com>
2021-04-17 19:03:28 -0600

    almost done flightsList

--- src/main/webapp/js/flight_list.js
+++ src/main/webapp/js/flight_list.js
@@ -1,4 +1,4 @@
-import {WSflight, typeOfFlight, datesOfFlight} from './utils/flight.js'
+import {WSflight, typeOfFlight, datesOfFlight, toDate} from './utils/flight.js'
 
 let wsflight, GlobalFlight;
 
@@ -6,15 +6,31 @@ $(load)
 
 function load(){
     wsflight = WSflight()
-    wsflight.onmessage = function(event){processMsg(event)}
+    wsflight.onmessage = function(event){processMsgFlight(event)}
     $("#deleteFlight").on("click", ()=>deleteFlight(GlobalFlight))
+    $("#addNewFlightBtn").on("click", ()=>showFlighInfoModal(null))
+    $("#flightRoute").on("change", ()=>updateASeats())
+    $("#addFlightB").on("click", ()=>addFlight())
+    $("#updateFlightB").on("click", ()=>updateFlight(GlobalFlight))
+}
+
+async function updateASeats(){
+    let id = $("#flightRoute").val()
+    if(id==="") {
+        $("#aSeats").val(0)
+        return;
+    }
+    let response = await fetch("/AirlineJK/routes/get?id="+id)
+    let route = await response.json()
+    let planeType = route.airplaneId.airplaneType
+    $("#aSeats").val(planeType.rowQ * planeType.columnQ)
 }
 
 function sendMsg(obj){
     wsflight.send(JSON.stringify(obj))
 }
 
-function processMsg(evt){
+function processMsgFlight(evt){
     let request = JSON.parse(evt.data)
     switch(request.type){
         case 'all': 
@@ -23,6 +39,8 @@ function processMsg(evt){
             flightTr(request.content, 'upd'); break;
         case 'delete': 
             flightTr(request.content, 'del'); break;
+        case 'add': 
+            flightTr(request.content, 'add'); break;
     }
 }
 
@@ -50,16 +68,50 @@ function flightTr(f, type){
         "<td class='col-1'>"+
             "<i id='del-"+f.id+"' style='cursor: pointer;' class='fas fa-ban fa-l-red'></i>"+
         "</td>";
-    if(type === 'new'){
+    if(type === 'new' || type === 'add'){
         let tbody = $("#flightsList")
         let tr = $('<tr id="tr-'+f.id+'" class="d-flex" />')
         tr.html(trContent)
-        tr.find("#edit-"+f.id).on("click",()=>showEditFlightModal(f))
+        tr.find("#edit-"+f.id).on("click",()=>showFlighInfoModal(f))
         tr.find("#del-"+f.id).on("click",()=>showDeleteFlightModal(f))
-        tbody.append(tr)
+        if(type === 'add')
+            tbody.prepend(tr)
+        else
+            tbody.append(tr)
+    }
+    if(type === 'upd'){
+        let tr = $("#tr-"+f.id)
+        tr.html(trContent)
+        tr.find("#edit-"+f.id).on("click",()=>showFlighInfoModal(f))
+        tr.find("#del-"+f.id).on("click",()=>showDeleteFlightModal(f))
     }
 }
 
+//<option value="CR-USA">San Jose, CR - Nueva York, USA </option>
+function showFlighInfoModal(f){
+    if(f === null){
+        $('#FlightForm').get(0).reset()
+        $("#flightRoute").prop('disabled', false)
+        $("#flightModalTittle").text("Agregar Vuelo")
+        $("#updateFlightB").prop("hidden",true)
+        $("#addFlightB").prop("hidden", false)
+    }else{
+        GlobalFlight = f
+        $("#flightModalTittle").text("Editar Vuelo")
+        $("#flightRoute").val(f.route.id); $("#flightRoute").prop('disabled', true)
+        $('#departureDate').val(toDate(f.departureDate))
+        if(f.returnDate !== undefined)
+            $('#returnDatee').val(toDate(f.returnDate))
+        else
+            $("#returnDatee").val("")
+        $("#aSeats").val(f.availableSeats)
+        $("#tPrice").val(f.price)
+        $("#discount").val(f.discount * 100)
+        $("#updateFlightB").prop("hidden", false)
+        $("#addFlightB").prop("hidden", true)
+    }
+    $("#addFlightModal").modal("show")
+}
 
 function showDeleteFlightModal(f){
     GlobalFlight = f
@@ -77,5 +129,43 @@ function deleteFlight(flight){
     }
     sendMsg(msg)
     $("#confirmDeleteModal").modal("hide")
+    $("#successModal").modal("show")
+}
+
+function updateFlight(flight){
+    let f = {
+        "id":flight.id,
+        "availableSeats": flight.availableSeats,
+        "price": $("#tPrice").val(),
+        "discount": $("#discount").val(),
+        "departureDate": new Date($('#departureDate').val()),
+        "returnDate": $("#returnDatee").val() !== ""? new Date($("#returnDatee").val()) : null,
+        "route" : flight.route
+    }
+    let msg = {
+        "type":"update",
+        "content":JSON.stringify(f)
+    }
+    sendMsg(msg)
+    $("#addFlightModal").modal("hide")
+    $("#successModal").modal("show")
+}
+
+function addFlight(){
+    let flight = {
+        "availableSeats":$("#aSeats").val(),
+        "price": $("#tPrice").val(),
+        "discount": $("#discount").val(),
+        "departureDate": new Date($('#departureDate').val()),
+        "returnDate": $("#returnDatee").val() !== ""? new Date($("#returnDatee").val()) : null,
+        "route" :{ "id": $("#flightRoute").val() }
+    }
+    console.log(flight)
+    let msg = {
+        "type":"add",
+        "content":JSON.stringify(flight)
+    }
+    sendMsg(msg)
+    $("#addFlightModal").modal("hide")
     $("#successModal").modal("show")
 }
--- src/main/webapp/js/utils/flight.js
+++ src/main/webapp/js/utils/flight.js
@@ -8,6 +8,19 @@ function typeOfFlight(f){
     return 'Ida y Vuelta'
 }
 
+function toDate(date){
+    let months = {
+       ene: "01",feb: "02",mar: "03",
+       abr: "04",may: "05",jun: "06",
+       jul: "07",ago: "08",sep: "09",
+       oct: "10",nov: "11",dic: "12"
+    }
+    //may. 23, 2021
+    let [month, ...r] = date.split(". ")
+    let [day, year] = r[0].split(", ")
+    return year+"-"+months[month]+"-"+day
+}
+
 function datesOfFlight(f){
     let type = typeOfFlight(f)
     switch(type){
@@ -18,6 +31,6 @@ function datesOfFlight(f){
     }
 }
 
-export {WSflight, typeOfFlight,datesOfFlight}
+export {WSflight, typeOfFlight, datesOfFlight, toDate}
 
 
