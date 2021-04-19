686581f0ea66c3201c3ff83ddf02d247f10ca823
KevinFloresG <kevinfg1413@gmail.com>
2021-04-18 21:54:53 -0600

    Function Ended

--- src/main/webapp/js/user_reservations.js
+++ src/main/webapp/js/user_reservations.js
@@ -16,7 +16,6 @@ function showReservations(reservations){
     let tbody = $("#userReservationList"), auxTr
     tbody.html("")
     reservations.forEach(r => {
-        console.log(r)
         auxTr = $("<tr class='d-flex' />")
         auxTr.html(
             "<td class='col-4'>"+r.flightInfo+"</td>"+									
@@ -43,6 +42,26 @@ async function showReservationModal(r_id){
     $("#price").val(r.totalPrice)
     $("#payType").val(r.typeOfPayment.description)
     $("#state").val(checkinStatus(r))
-    $("#checkedInQ").val(r.checkedInQuantity)
+    listTickets(r_id)
     $("#reservationModal").modal("show")
-} 
+}
+
+async function listTickets(r_id){
+    let response = await fetch("/AirlineJK/tickets/get/reservation?id="+r_id)
+    let list = await response.json()
+    let tbody = $("#rTicketsList"), tr;
+    tbody.html("")
+    if(list.length === 0){
+        tbody.html("<tr><td class='font-italic font-weight-bold'>DEBES REALIZAR EL CHECK-IN</td></tr>")
+    }else{
+        list.forEach(t=>{
+            tr = $('<tr class="d-flex" />')
+            tr.html(
+                '<td scope="col" class="col-4">'+t.id+'</td>'+
+                '<td scope="col" class="col-4">'+t.rowN+'</td>'+
+                '<td scope="col" class="col-4">'+String.fromCharCode(65 + t.columnN)+'</td>'
+            )
+            tbody.append(tr)
+        })
+    }
+}
